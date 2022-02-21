ff7428e164534454a7e34b6c82c8dda47a4ae5eb
Michiel Rensen <michiel.rensen@lightspeedhq.com>
2022-02-22 00:57:04 +0100

    Removing jinn from default installation as it's too specific

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -16,9 +16,9 @@ then
 fi
 
 # Install sane brew packages
-brew install ansible ansible-lint antigen argocd awscli bat circleci shopify/shopify/ejson gh git-crypt helm heroku-node htop hub istioctl lightspeedretail/jinn/jinn jinn-app jinn-cd jinn-gaia jinn-kumo jinn-queryr jq k9s kns kubectx mkcert molecule nmap packer php@7.4 rbenv rbenv-chefdk rsync ruby ruby-build shellcheck telnet terraform tmate vault vim wget
+brew install ansible ansible-lint antigen argocd awscli bat circleci shopify/shopify/ejson gh git-crypt helm heroku-node htop hub istioctl jq k9s kns kubectx mkcert molecule nmap packer php@7.4 rbenv rbenv-chefdk rsync ruby ruby-build shellcheck telnet terraform tmate vault vim wget
 
 # Install sane brew cask packages
-brew cask install 1password brave-browser docker dropbox font-hack font-hack-nerd-font font-meslo-for-powerline font-meslo-lg-dz font-meslo-lg-nerd-font gdisk google-cloud-sdk gpg-suite iterm2 keybase sequel-ace slack stats vagrant virtualbox zoom
+brew install --cask 1password brave-browser docker dropbox font-hack font-hack-nerd-font font-meslo-for-powerline font-meslo-lg-dz font-meslo-lg-nerd-font gdisk google-cloud-sdk gpg-suite iterm2 keybase sequel-ace slack stats vagrant virtualbox zoom
 
 exit 0
