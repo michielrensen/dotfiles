d6863e8be084b2c8478b5b6bb51a031529e6ad47
Michiel Rensen <michiel.rensen@lightspeedhq.com>
2022-02-22 00:33:19 +0100

    Fixing brew install packages syntax error

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -16,63 +16,9 @@ then
 fi
 
 # Install sane brew packages
-brew install (ansible
-ansible-lint
-argocd
-awscli
-bat
-circleci
-ejson
-gh
-git-crypt
-helm
-heroku-node
-htop
-hub
-istioctl
-jinn
-jinn-app
-jinn-cd
-jinn-gaia
-jinn-kumo
-jinn-queryr
-jq
-k9s
-kns
-kubectx
-mkcert
-molecule
-nmap
-packer
-php@7.4
-rbenv
-rbenv-chefdk
-rsync
-ruby
-ruby-build
-shellcheck
-telnet
-terraform
-tmate
-vault
-vim
-wget)
+brew install ansible ansible-lint argocd awscli bat circleci ejson gh git-crypt helm heroku-node htop hub istioctl jinn jinn-app jinn-cd jinn-gaia jinn-kumo jinn-queryr jq k9s kns kubectx mkcert molecule nmap packer php@7.4 rbenv rbenv-chefdk rsync ruby ruby-build shellcheck telnet terraform tmate vault vim wget
 
 # Install sane brew cask packages
-brew cask install (docker
-font-hack
-font-hack-nerd-font
-font-meslo-for-powerline
-font-meslo-lg-dz
-font-meslo-lg-nerd-font
-gdisk
-google-cloud-sdk
-gpg-suite
-keybase
-sequel-ace
-stats
-vagrant
-virtualbox
-)
+brew cask install docker font-hack font-hack-nerd-font font-meslo-for-powerline font-meslo-lg-dz font-meslo-lg-nerd-font gdisk google-cloud-sdk gpg-suite keybase sequel-ace stats vagrant virtualbox
 
 exit 0
