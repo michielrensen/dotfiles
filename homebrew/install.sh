d7015d73fa7847e3e21ec7f14d739172b7b04f84
Michiel Rensen <michiel.rensen@lightspeedhq.com>
2022-02-22 00:47:18 +0100

    Including taps where appropriate

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -16,9 +16,9 @@ then
 fi
 
 # Install sane brew packages
-brew install ansible ansible-lint argocd awscli bat circleci ejson gh git-crypt helm heroku-node htop hub istioctl jinn jinn-app jinn-cd jinn-gaia jinn-kumo jinn-queryr jq k9s kns kubectx mkcert molecule nmap packer php@7.4 rbenv rbenv-chefdk rsync ruby ruby-build shellcheck telnet terraform tmate vault vim wget
+brew install ansible ansible-lint argocd awscli bat circleci shopify/shopify/ejson gh git-crypt helm heroku-node htop hub istioctl lightspeedretail/jinn/jinn jinn-app jinn-cd jinn-gaia jinn-kumo jinn-queryr jq k9s kns kubectx mkcert molecule nmap packer php@7.4 rbenv rbenv-chefdk rsync ruby ruby-build shellcheck telnet terraform tmate vault vim wget
 
 # Install sane brew cask packages
-brew cask install docker font-hack font-hack-nerd-font font-meslo-for-powerline font-meslo-lg-dz font-meslo-lg-nerd-font gdisk google-cloud-sdk gpg-suite keybase sequel-ace stats vagrant virtualbox
+brew cask install 1password brave-browser docker dropbox font-hack font-hack-nerd-font font-meslo-for-powerline font-meslo-lg-dz font-meslo-lg-nerd-font gdisk google-cloud-sdk gpg-suite iterm2 keybase sequel-ace slack stats vagrant virtualbox zoom
 
 exit 0
