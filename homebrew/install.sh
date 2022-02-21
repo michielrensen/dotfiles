d56e22729c9417eed26caa5e044f79353f736214
Michiel Rensen <michiel.rensen@lightspeedhq.com>
2022-02-21 23:57:01 +0100

    Initial basic commit

---
+++ homebrew/install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#
+# Homebrew
+#
+# This installs some of the common dependencies needed (or at least desired)
+# using Homebrew.
+
+# Check for Homebrew
+if test ! $(which brew)
+then
+  echo "  Installing Homebrew for you."
+
+  # Install the correct homebrew for each OS type
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+fi
+
+exit 0
---
+++ zsh/install.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+#
+# Antigen
+curl -L git.io/antigen > /usr/local/share/antigen/antigen.zsh
